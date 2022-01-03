1e04b4adb79a9897f779eefcdd658da1670d1a11
Jiwon <sjiwon9303@gmail.com>
2022-01-03 19:51:34 +0900

    footer

--- src/components/main/Item.js
+++ src/components/main/Item.js
@@ -6,7 +6,7 @@ return (
   <div class="container"> 
       <div class="justify-content-center row"> 
           <div class="col-lg-6 col-md-6 py-3"> 
-              <div class="position-relative text-dark"> 
+              <div class="position-relative text-dark "> 
                   <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
                   <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
@@ -18,6 +18,8 @@ return (
               <div class="position-relative text-dark"> 
                   <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
                   <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
+                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
+                  <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
                   </div>                     
               </div>                 
@@ -25,6 +27,8 @@ return (
           <div class="col-lg-3 col-md-3 py-3"> 
               <div class="position-relative text-dark"> 
                   <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
+                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
+                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
                   <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
                   </div>                     
