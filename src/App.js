cdfdc252d4b7dbb6226891869cf4f33a64067bc4
commGom <hyunwoo921129@gmail.com>
2022-01-03 17:08:37 +0900

    Router 체크 중

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import Signin from './components/user/Signin';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Main from './components/main/Main';
 import Book from './components/book/Book';
+import UserUpdate from './components/mypage/UserUpdate';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main></Main>}></Route>
+          <Route path="/" element={<Main></Main>}>
+            <Route path="*"></Route>
+          </Route>
           <Route path="/user/signin" element={<Signin></Signin>}></Route>
           <Route path="/book" element={<Book></Book>}></Route>
+          <Route path="/user/update" element={<UserUpdate></UserUpdate>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 import Book from '../book/Book';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
+import UserUpdate from '../mypage/UserUpdate';
+import Signin from '../user/Signin';
 
 const Main = () => {
   return (
     <div>
-
+      <Link to="/sub">폼</Link>
+      <Link to="/user/signin">폼</Link>
+      <Routes>
+        <Route path='/sub' element={<UserUpdate></UserUpdate>}></Route>
+      </Routes>
       <Header></Header>
       <h1>메인페이지</h1>
       <Footer></Footer>
