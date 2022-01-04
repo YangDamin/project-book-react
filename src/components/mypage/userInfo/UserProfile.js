8c376d1d7ff8541e7fde49dda9afb2ed793feefb
commGom <hyunwoo921129@gmail.com>
2022-01-04 09:17:35 +0900

    Userprofile UserUpdate 전반적으로 기능 구현 완료

--- src/components/mypage/userInfo/UserProfile.js
+++ src/components/mypage/userInfo/UserProfile.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 
 
 const UserProfile = () => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const result = axios({
+      url: 'http://localhost:8080/mypage/userUpdate',
+      method: 'get',
+      params: { userId: 'phyw1129@naver.com', userPassword: 1234 }
+    });
+    result.then((res) => {
+      console.log(res);
+      console.log(res.data);
+      setUser(res.data);
+    }, []);//deps
+  }, []);
   return (
     <div class="col-lg-6" id="contents">
       <h3> My Profile</h3>
@@ -9,37 +24,34 @@ const UserProfile = () => {
       <div class="mt-3  bg-light bg-gradient">
         <div>
           <label>
-            <p class="my-3 p-1 fs-4 "><b class="text-black-60">아이디 :</b><span>&nbsp;&nbsp;아이디~</span>
+            <p class="my-3 p-1 fs-4 "><b class="text-black-60">아이디 :&nbsp;&nbsp;</b><span>{user.email}</span>
             </p>
           </label>
         </div>
-
-
         <label>
-          <p class="my-3 p-1 fs-4"><b class="text-black-60">생년월일 :</b><span>&nbsp;&nbsp;
-            2021-01-03
+          <p class="my-3 p-1 fs-4"><b class="text-black-60">생년월일 :&nbsp;&nbsp;</b><span>
+            {user.birth}
           </span></p>
 
         </label>
         <br />
         <label>
-          <p class="my-3 p-1 fs-4"><b class="text-black-60">이름 :</b><span>&nbsp;&nbsp;한국 표준</span></p>
+          <p class="my-3 p-1 fs-4"><b class="text-black-60">이름 :&nbsp;&nbsp;</b><span>{user.name}</span></p>
         </label>
         <br />
         <label>
-          <p class="my-3 p-1 fs-4"><b>휴대전화 번호 :</b><span class="fs-4">&nbsp;&nbsp;010-1234-5678</span></p>
+          <p class="my-3 p-1 fs-4"><b>휴대전화 번호 :&nbsp;&nbsp;</b><span class="fs-4">{user.phone}</span></p>
         </label>
         <br />
         <label>
           <p class="my-3 p-1 fs-4"><b>주소 :</b></p>
-          <span class="fs-4">&nbsp;&nbsp;서울특별시 금천구 가산디지털단지역</span>
+          <span class="fs-4">&nbsp;&nbsp;{user.address}</span>
         </label>
         <br />
         <label>
           <p class="my-3 p-1 fs-4"><b>가입일 :</b><span class="fs-4">&nbsp;&nbsp;
-            가입일
+            {user.createdDate}
           </span></p>
-
         </label>
       </div>
 
--- src/components/mypage/userInfo/UserUpdate.js
+++ src/components/mypage/userInfo/UserUpdate.js
@@ -1,61 +1,101 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 
 const UserUpdate = () => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const result = axios({
+      url: 'http://localhost:8080/mypage/userUpdate',
+      method: 'get',
+      params: { userId: 'phyw1129@naver.com', userPassword: 1234 }
+    });
+    result.then((res) => {
+      console.log(res);
+      console.log(res.data);
+      setUser(res.data);
+    });
+  }, []);//deps
   return (
     <div class="col-lg-6">
-      <h4 class="mb-3">회원 정보 수정</h4>
+      <div className="row">
+        <div className="col-lg-4">
+          <h4 class="mb-3">회원 정보 수정</h4>
+        </div>
+        <div className="col-lg-6"></div>
+        <div className="col-lg-2">
+          <button type="button" className="btn-lg btn-outline-secondary"
+            onClick={() => {
+              const formData = new FormData();
+              formData.append("userId", "phyw1129@naver.com");
+              formData.append("userPassword", "1234");
+              formData.append("password", document.getElementById("password").value);
+              formData.append("phone", document.getElementById("phone").value);
+              formData.append("address", document.getElementById("address").value);
+              const result = axios({
+                url: 'http://localhost:8080/mypage/userUpdate',
+                method: 'post',
+                data: formData
+              });
+              result.then((res) => {
+                console.log(res);
+                console.log(res.data);
+
+              });
+            }}>변경하기</button>
+        </div>
+      </div>
+
       <form class="needs-validation" novalidate="">
         <div class="row">
           <div className="mb-3">
             <label for="name">이름</label>
-            <input type="text" class="form-control" id="name" placeholder="" value="한국표준" required readOnly />
+            <input type="text" class="form-control" id="name" placeholder="" value={user.name} required
+              onChange={(e) => { document.getElementById("name").value = e.target.value }} />
             <div class="invalid-feedback"> Valid first name is required. </div>
           </div>
         </div>
 
         <div class="mb-3">
           <label for="email">Email <span class="text-muted">(User ID)</span></label>
-          <input type="email" class="form-control" id="email" value="you@example.com" readOnly />
+          <input type="email" class="form-control" id="email" value={user.email} readOnly />
           <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
         </div>
         <div class="mb-1">
           <label for="password">Password <span class="text-muted"></span></label>
-          <input type="password" class="form-control" id="password" placeholder="" />
+          <input type="password" class="form-control" id="password" onChange={(e) => { document.getElementById("password").value = e.target.value }} />
           <div class="invalid-feedback"> Please enter a valid email address for shipping updates. </div>
         </div>
         <div class="mb-3">
           <label for="password_check">Password <span class="text-muted">(Check)</span></label>
-          <input type="password" class="form-control" id="password_check" placeholder="" />
+          <input type="password" class="form-control" id="password_check" onChange={(e) => { document.getElementById("password_check").value = e.target.value }} />
           <div class="invalid-feedback"> Please enter a valid password. </div>
         </div>
 
         <div class="mb-3">
           <label for="birth">Birth <span class="text-muted"></span></label>
-          <input type="date" class="form-control" id="birth" readOnly value="2021-01-03" />
+          <input type="date" class="form-control" id="birth" readOnly value={user.birth} />
           <div class="invalid-feedback"> Please enter a valid birth </div>
         </div>
         <div class="mb-3">
           <label for="phone">Phone <span class="text-muted"></span></label>
-          <input type="tel" class="form-control" id="phone" placeholder="02-803-1234" />
+          <input type="tel" class="form-control" id="phone" value={user.phone} onChange={(e) => { document.getElementById("phone").value = e.target.value }} />
           <div class="invalid-feedback"> Please enter a valid phone number for shipping updates. </div>
         </div>
 
-        <div class="mb-3">
+        <div class="mb-1">
           <label for="address">Address</label>
-          <input type="text" class="form-control" id="address" placeholder="금천구 가산디지털단지역" required />
+          <input type="text" class="form-control" id="address" value={user.address} required onChange={(e) => { document.getElementById("address").value = e.target.value }} />
           <div class="invalid-feedback"> Please enter your shipping address. </div>
         </div>
-        <div class="mb-3">
+        <div class="mb-1">
           <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-          <input type="text" class="form-control" id="address2" placeholder="Apartment or suite" />
+          <input type="text" class="form-control" id="address2" value={user.address} required onChange={(e) => { document.getElementById("address2").value = e.target.value }} />
         </div>
-        <div class="row">
-          <div class="col-md-3 mb-3">
-            <label for="post_code">Post Code</label>
-            <input type="text" class="form-control" id="post_code" placeholder="" required />
-            <div class="invalid-feedback"> Post Code required. </div>
-          </div>
+        <div class="col-md-3 mb-5">
+          <label for="post_code">Post Code</label>
+          <input type="text" class="form-control" id="post_code" placeholder="" required />
+          <div class="invalid-feedback"> Post Code required. </div>
         </div>
       </form>
     </div>
