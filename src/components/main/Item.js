b77a65c4ece6ec2f9ed086bd60b3cf49bbddde4c
Jiwon <sjiwon9303@gmail.com>
2022-01-04 18:08:22 +0900

    bookdetail+footer+Item

--- src/components/main/Item.js
+++ src/components/main/Item.js
@@ -7,8 +7,8 @@ return (
       <div class="justify-content-center row"> 
           <div class="col-lg-6 col-md-6 py-3"> 
               <div class="position-relative text-dark "> 
-                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
-                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
+                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="456" height="456"/>
+                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="456"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
                   </div>                     
               </div>                 
@@ -16,9 +16,9 @@ return (
 
           <div class="col-lg-3 col-md-3 py-3 "> 
               <div class="position-relative text-dark"> 
-                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
-                  <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
-                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
+                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="228" height="228"/>
+                  <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="228" height="228"/>
+                  <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="228" height="228"/>
                   <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
                   </div>                     
@@ -26,8 +26,8 @@ return (
           </div>
           <div class="col-lg-3 col-md-3 py-3"> 
               <div class="position-relative text-dark"> 
-                  <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
-                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
+                  <img src="image/main_img/book7.png" class="img-fluid w-100" alt="Product image" width="228" height="228"/>
+                  <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="428" height="228"/>
                   <img src="image/main_img/book4.png" class="img-fluid w-100" alt="Product image" width="280" height="200"/>
                   <img src="image/main_img/book3.png" class="img-fluid w-100" alt="Product image" width="400" height="500"/>
                   <div class="bottom-0 end-0  pb-5 pe-4 position-absolute ps-4 start-0">
