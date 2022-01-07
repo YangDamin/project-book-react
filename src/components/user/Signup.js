79c1d725d4e1ad7f4acffc26b8b6da157120ca23
YangDamin <cxz26@naver.com>
2022-01-07 11:07:55 +0900

    회원가입 디자인 수정

--- src/components/user/Signup.js
+++ src/components/user/Signup.js
@@ -32,7 +32,7 @@ const Signup = () => {
     setAddress(data.zonecode);
     setAddressDetail(fullAddr);
     setIsOpenPost(false);
-  };  
+  };
 
   const postCodeStyle = {
 
@@ -51,108 +51,108 @@ const Signup = () => {
   return (
 
 
-    <div class="container h-100">
-    		<div class="row h-100">
-				<div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
-					<div class="d-table-cell align-middle">
+    <div class="container h-100"  id="nanum_font">
+      <div class="row h-100">
+        <div class="col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100">
+          <div class="d-table-cell align-middle">
 
-						<div class="text-center mt-4">
-							<h1 class="h2 mt-5 mb-4">회원 가입</h1>
-						</div>
-
-						<div class="card">
-							<div class="card-body">
-								<div class="m-sm-4">
-									<form>
-										<div class="form-group">
-											<label>Name</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" type="text" name="name" placeholder="Enter your name" required>
-										</input></div>
-                  
-										<div class="form-group">
-											<label>Email</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" type="email" name="email" placeholder="Enter your email" required>
-										</input>
-                    </div>
-                    <div class="form-group">
-											<label>Password</label>
-											<input class="form-control form-control-lg" type="password" name="password" placeholder="Enter password" required>
-										</input>
-                    </div>
-										<div class="form-group">
-											<label>birth</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" placeholder="birth" type="date" autoComplete="current" name="birth" id="birth" required>
-										</input>
-                    </div>
-                    <div class="form-group">
-											<label>phone</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" type="phone" name="phone" placeholder="Enter your phone" required>
-										</input>
-                    </div>
-                    <div class="form-group">
-											<label>Postcode</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" placeholder="우편번호(주소검색버튼)" readonly value={address} type="postcode"  autoComplete="current" name="postcode" id="sample4_postcode" required>
-										</input>
-                    </div>
-                    <div class="form-group">
-											<label>Address</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" placeholder="주소(주소검색버튼)" readonly value={addressDetail} type="address1" autoComplete="current" name="address1" id="sample5_address1" required>
-										</input>
-                    </div>
-                    <div className={styles.button_area}>
-                    <Button className={styles.button} size='large' onClick={(e) => {
-                     setIsOpenPost(true);
-                     }} >주소검색</Button>
-                     </div>
-                      {isOpenPost  ? (
-                      <DaumPostcode style={postCodeStyle} autoClose onComplete={onCompletePost } />
-                       ) : null}
-
-                      <div class="form-group">
-											<label>Address2</label><span className={styles.required}>*</span>
-											<input class="form-control form-control-lg" placeholder="상세주소" type="address2" autoComplete="current" name="address2" id="sample5_address2" required>
-							  			</input>
-                      </div>
-     
-										
-										
-                    <div className={styles.button_area}>
-                 <Button className={styles.button} size="large" onClick={(e) => {
-                    e.preventDefault();
-                const email = document.querySelector('[name=email]').value;
-                const formData = new FormData();
-                formData.append('email', email);
-                formData.append('password', document.querySelector('[name=password]').value);
-                formData.append('birth', document.querySelector('[name=birth]').value);
-                formData.append('name', document.querySelector('[name=name]').value);
-                formData.append('phone', document.querySelector('[name=phone]').value);
-                formData.append('postcode', document.querySelector('[name=postcode]').value);
-                formData.append('address1', document.querySelector('[name=address1]').value);
-                formData.append('address2', document.querySelector('[name=address2]').value);
-                axios({
-                    url:'http://localhost:8080/user/signup',
-                    method: 'post',
-                    data: formData
-                }).then((res) => {
-                    console.log(res.data);
-
-                    if(res.data.code != 200) {
-                        alert('🤦‍♂️회원가입 승인불가🤦‍♂️');
-                    } else {
-                        alert('👊회원가입 완료👊');
-                    }
-                });
-              }}>회원가입</Button>
+            <div class="text-center mt-4">
+              <h3 class="h3 mt-5 mb-4">회원 가입</h3>
             </div>
-									</form>
-								</div>
-							</div>
-						</div>
 
-					</div>
-				</div>
-			</div>
-		</div>
+            <div class="card">
+              <div class="card-body">
+                <div class="m-sm-4">
+                  <form>
+                    <div class="form-group mb-3">
+                      <label>Name</label><span className={styles.required}>*</span>
+                      <input class="form-control" type="text" name="name" placeholder="Enter your name" required>
+                      </input></div>
+
+                    <div class="form-group mb-3">
+                      <label>Email</label><span className={styles.required}>*</span>
+                      <input class="form-control" type="email" name="email" placeholder="Enter your email" required>
+                      </input>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label>Password</label><span className={styles.required}>*</span>
+                      <input class="form-control" type="password" name="password" placeholder="Enter password" required>
+                      </input>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label>Birth</label><span className={styles.required}>*</span>
+                      <input class="form-control" placeholder="birth" type="date" autoComplete="current" name="birth" id="birth" required>
+                      </input>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label>Phone</label><span className={styles.required}>*</span>
+                      <input class="form-control" type="phone" name="phone" placeholder="Enter your phone" required>
+                      </input>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label>Postcode</label><span className={styles.required}>*</span>
+                      <input class="form-control" placeholder="우편번호(주소검색버튼)" readonly value={address} type="postcode" autoComplete="current" name="postcode" id="sample4_postcode" required>
+                      </input>
+                    </div>
+                    <div class="form-group mb-3">
+                      <label>Address</label><span className={styles.required}>*</span>
+                      <input class="form-control" placeholder="주소(주소검색버튼)" readonly value={addressDetail} type="address1" autoComplete="current" name="address1" id="sample5_address1" required>
+                      </input>
+                    </div>
+                    <div className={styles.button_area}>
+                      <Button className={styles.button} size='large' onClick={(e) => {
+                        setIsOpenPost(true);
+                      }} >주소검색</Button>
+                    </div>
+                    {isOpenPost ? (
+                      <DaumPostcode style={postCodeStyle} autoClose onComplete={onCompletePost} />
+                    ) : null}
+
+                    <div class="form-group mb-3 mt-3">
+                      <label>Address2</label><span className={styles.required}>*</span>
+                      <input class="form-control" placeholder="상세주소" type="address2" autoComplete="current" name="address2" id="sample5_address2" required>
+                      </input>
+                    </div>
+
+
+
+                    <div className={styles.button_area}>
+                      <Button className={styles.button} size="large" onClick={(e) => {
+                        e.preventDefault();
+                        const email = document.querySelector('[name=email]').value;
+                        const formData = new FormData();
+                        formData.append('email', email);
+                        formData.append('password', document.querySelector('[name=password]').value);
+                        formData.append('birth', document.querySelector('[name=birth]').value);
+                        formData.append('name', document.querySelector('[name=name]').value);
+                        formData.append('phone', document.querySelector('[name=phone]').value);
+                        formData.append('postcode', document.querySelector('[name=postcode]').value);
+                        formData.append('address1', document.querySelector('[name=address1]').value);
+                        formData.append('address2', document.querySelector('[name=address2]').value);
+                        axios({
+                          url: 'http://localhost:8080/user/signup',
+                          method: 'post',
+                          data: formData
+                        }).then((res) => {
+                          console.log(res.data);
+
+                          if (res.data.code != 200) {
+                            alert('🤦‍♂️회원가입 승인불가🤦‍♂️');
+                          } else {
+                            alert('👊회원가입 완료👊');
+                          }
+                        });
+                      }}>회원가입</Button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
 
   );
 };
